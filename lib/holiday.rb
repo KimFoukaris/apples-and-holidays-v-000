0329c17137a74bd5555acca1f94dbe3f3e38ad7c
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 00:05:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holidays_hash|
     holidays_hash.each do |holiday, supplies|
       if holiday == :winter
+        binding.pry
         winter_supplies << supplies
       end
     end
