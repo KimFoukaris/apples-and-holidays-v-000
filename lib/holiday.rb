a80b447b2309f2b02ed318067dc462f7680c0fb4
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 00:27:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_hash|
     puts "#{season.capitalize}:"
       holidays_hash.each do |holiday, supplies|
-          puts "   #{holiday.captalize}: #{supplies}"
+          puts "   #{holiday.capitalize}: #{supplies}"
       end
     end
   end
