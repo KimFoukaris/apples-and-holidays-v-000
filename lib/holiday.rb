91d32d869e9bd3289accdca82ee8e41d429b8838
Kim Foukaris <kimfoukaris@outlook.com>
2018-01-30 01:03:28 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,9 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
       holidays_hash.each do |holiday, supplies|
           holiday_string = holiday.to_s
           holiday_array = holiday_string.split
-          holiday_array.each do |word|
-            holiday_array_capitalized << word.capitalize!
-          end
+          holiday_array.each {|word| holiday_array_capitalized << word.capitalize!}
           holiday_capitalized = holiday_array_capitalized.join(" ")
           supply_list = supplies.join(", ")
           puts "  #{holiday_capitalized}: #{supply_list}"
